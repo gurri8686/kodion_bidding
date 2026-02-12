8d05c11781032799441945a5f04c8391ee129707
Maan Singh <maan.6050@gmail.com>
2026-02-12 15:21:41 +0530

    route

--- next-app/app/api/auth/login/route.ts
+++ next-app/app/api/auth/login/route.ts
@@ -1,45 +1,14 @@
 import { NextResponse } from "next/server";
 import pool from "@/lib/db";
-import bcrypt from "bcryptjs";
 
 export async function POST(req: Request) {
   try {
-    const { email, password } = await req.json();
-
-    const [rows]: any = await pool.query(
-      "SELECT * FROM users WHERE email = ?",
-      [email]
-    );
-
-    if (rows.length === 0) {
-      return NextResponse.json(
-        { message: "User not found" },
-        { status: 400 }
-      );
-    }
-
-    const user = rows[0];
-
-    const isMatch = password === user.password;
-
-    if (!isMatch) {
-      return NextResponse.json(
-        { message: "Invalid password" },
-        { status: 400 }
-      );
-    }
-
-    return NextResponse.json({
-      message: "Login successful",
-      user: {
-        id: user.id,
-        email: user.email,
-      },
-    });
-  } catch (error) {
-    console.error(error);
+    const [rows] = await pool.query("SELECT 1");
+    return NextResponse.json({ message: "DB Working", rows });
+  } catch (error: any) {
+    console.error("FULL ERROR:", error);
     return NextResponse.json(
-      { message: "Server error" },
+      { message: "Server error", error: error.message },
       { status: 500 }
     );
   }
