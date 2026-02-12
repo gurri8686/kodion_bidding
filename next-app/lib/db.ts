f20339a7332a372617db4ad6c6101c00def6c7aa
Maan Singh <maan.6050@gmail.com>
2026-02-12 16:07:50 +0530

    changesDatabase

--- next-app/lib/db.ts
+++ next-app/lib/db.ts
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
+  host: process.env.MYSQL_DB_HOST || "",
+  user: process.env.MYSQL_DB_USER || "",
+  password: process.env.MYSQL_DB_PASSWORD || "",
+  database: process.env.MYSQL_DB_NAME || "",
+  port: Number(process.env.MYSQL_DB_PORT) || 3306,
   ssl: {
     rejectUnauthorized: false,
   },
