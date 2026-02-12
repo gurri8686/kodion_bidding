6594e95763beda507be22e2df9bb3aaa862cf51b
Maan Singh <maan.6050@gmail.com>
2026-02-12 15:13:27 +0530

    loginChange

--- next-app/app/api/auth/login/route.ts
+++ next-app/app/api/auth/login/route.ts
@@ -1,7 +1,46 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import pool from "@/lib/db";
+import bcrypt from "bcryptjs";
 
-export async function POST() {
-  return NextResponse.json({
-    message: 'Login API working on Vercel',
-  });
+export async function POST(req: Request) {
+  try {
+    const { email, password } = await req.json();
+
+    const [rows]: any = await pool.query(
+      "SELECT * FROM users WHERE email = ?",
+      [email]
+    );
+
+    if (rows.length === 0) {
+      return NextResponse.json(
+        { message: "User not found" },
+        { status: 400 }
+      );
+    }
+
+    const user = rows[0];
+
+    const isMatch = password === user.password;
+
+    if (!isMatch) {
+      return NextResponse.json(
+        { message: "Invalid password" },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json({
+      message: "Login successful",
+      user: {
+        id: user.id,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Server error" },
+      { status: 500 }
+    );
+  }
 }
